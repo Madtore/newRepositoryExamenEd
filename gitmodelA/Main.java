91a51ef28e2928819eef8e4773dccc347c62c9a8
Madtore <143751703+Madtore@users.noreply.github.com>
2024-03-06 19:47:57 +0100

    Update Main.java
    CambioEn Remoto

--- gitmodelA/Main.java
+++ gitmodelA/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		System.out.println("Hola mundo");
 		System.out.println("Hola a tod@s");
 		System.out.println("Cambio Rama Master");
+		System.out.println("Cambio en remoto");
 		
 	}
 
